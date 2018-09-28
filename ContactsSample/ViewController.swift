d818a9e148613d059d2b45a6f325ba9fafd0d871
shtnkgm <nakagami.shota@gmail.com>
2018-09-28 15:20:31 +0900

    fix

--- ContactsSample/ViewController.swift
+++ ContactsSample/ViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import Contacts
+import ContactsUI
 
 class ViewController: UIViewController {
-
+    private lazy var contactPickerViewController : CNContactPickerViewController = {
+       let vc = CNContactPickerViewController()
+        vc.delegate = self
+        vc.displayedPropertyKeys = [CNContactEmailAddressesKey]
+        vc.predicateForEnablingContact = NSPredicate(format: "emailAddresses.@count > 0")
+        return vc
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        present(contactPickerViewController, animated: true, completion: nil)
+    }
 }
 
+extension ViewController: CNContactPickerDelegate {
+    func contactPickerDidCancel(_ picker: CNContactPickerViewController) {
+        print("canceled")
+    }
+    
+    func contactPicker(_ picker: CNContactPickerViewController, didSelect contactProperty: CNContactProperty) {
+        print("\(contactProperty.key) selected")
+        guard let emailAddess = contactProperty.value as? String else { return }
+        print(emailAddess)
+    }
+}
